0aed79f8660023f5b0f8367c4c84a24f8d1697d5
meeky <98189596+mmsaki@users.noreply.github.com>
2023-03-13 18:37:13 -0500

    changed: dotenv configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [
+        async () => {
+          return configuration;
+        },
+      ],
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+<<<<<<< Updated upstream
 import { ethers } from 'ethers';
 import { BigNumber } from 'ethers/lib/ethers';
 //import * as dotenv from 'dotenv'
@@ -34,18 +35,66 @@ export class AppService {
       tokenizedBallotJson.abi,
       this.provider
     );
+=======
+import { BigNumber, ethers } from 'ethers';
+import * as tokenJson from './assets/MyToken.json';
+import * as ballotJson from './assets/TokenizedBallot.json';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { TransactionErrorDTO, TransactionResponseDTO } from './dtos';
+
+const API_KEY = process.env.ALCHEMY_API_KEY;
+const NETWORK = process.env.NETWORK;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const TKN_CONTRACT = process.env.MY_TKN_ADDRESS;
+const BALLOT_ADDRESS = process.env.BALLOT_ADDRESS;
+
+@Injectable()
+export class AppService {
+  provider: ethers.providers.BaseProvider;
+  tokenContract: ethers.Contract;
+  ballotContract: ethers.Contract;
+  network: string;
+  contractAddress: string;
+  ballotAddress: string;
+  Votes: Array<any>;
+  signer: ethers.Wallet;
+
+  constructor() {
+    if (TKN_CONTRACT && PRIVATE_KEY && API_KEY && NETWORK && BALLOT_ADDRESS) {
+      this.contractAddress = TKN_CONTRACT;
+      this.ballotAddress = BALLOT_ADDRESS;
+      this.provider = ethers.providers.getDefaultProvider(NETWORK, {
+        alchemy: API_KEY,
+      });
+      this.tokenContract = new ethers.Contract(
+        this.contractAddress,
+        tokenJson.abi,
+        this.provider
+      );
+      this.ballotContract = new ethers.Contract(
+        this.ballotAddress,
+        ballotJson.abi,
+        this.provider
+      );
+      this.signer = new ethers.Wallet(PRIVATE_KEY, this.provider);
+    }
+  }
+  counter = 0;
+  getHello(): string {
+    return 'Hello World!' + this.counter++;
+>>>>>>> Stashed changes
   }
 
-  getMyTokenContractAddress(): string {
-    return MY_TOKEN_CONTRACT_ADDRESS;
+  getTokenAddress(): string {
+    return this.contractAddress;
   }
-  
-  getTokenizedBallotContractAddress(): string {
-    return TOKENIZED_BALLOT_CONTRACT_ADDRESS;
+  getBallotAddress(): string {
+    return this.ballotAddress;
   }
 
   async getTotalSupply(): Promise<number> {
-    const totalSupplyBN: BigNumber = await this.myTokenContract.totalSupply();
+    const totalSupplyBN: BigNumber = await this.tokenContract.totalSupply();
     const totalSupplyString = ethers.utils.formatEther(totalSupplyBN);
     const totalSupplyNumber = parseFloat(totalSupplyString);
 
@@ -53,7 +102,7 @@ export class AppService {
   }
 
   async getAllowance(from: string, to: string): Promise<number> {
-    const allowanceBN: BigNumber = await this.myTokenContract.allowance(from, to);
+    const allowanceBN: BigNumber = await this.tokenContract.allowance(from, to);
     const allowanceString = ethers.utils.formatEther(allowanceBN);
     const allowanceNumber = parseFloat(allowanceString);
 
@@ -61,48 +110,61 @@ export class AppService {
   }
 
   async getTransactionStatus(txnHash: string): Promise<string> {
-    const txnReceipt = await this.myTokenContract.provider.getTransaction(txnHash);
+    const txnReceipt = await this.tokenContract.provider.getTransaction(
+      txnHash
+    );
     return txnReceipt?.blockNumber ? 'Success' : 'Fail';
   }
 
   async getTransactionReceipt(txnHash: string): Promise<string> {
-    const txnReceipt = await this.myTokenContract.provider.getTransaction(txnHash);
+    const txnReceipt = await this.tokenContract.provider.getTransaction(
+      txnHash
+    );
     return JSON.stringify(txnReceipt);
   }
 
-  async requestTokens(address: string, amount: number): Promise<TransactionResponseDTO | ErrorMessageDTO> {
+  async requestTokens(
+    address: string,
+    amount: number
+  ): Promise<TransactionResponseDTO | TransactionErrorDTO> {
     let txnError = null;
     let mintTxnReceipt = null;
 
     const amountBN = ethers.utils.parseEther(String(amount));
 
     try {
-      const mintTxn = await this.myTokenContract.connect(this.signer).mint(address, amountBN);
+      const mintTxn = await this.tokenContract
+        .connect(this.signer)
+        .mint(address, amountBN);
       mintTxnReceipt = await mintTxn.wait();
-    } catch (error) { 
+    } catch (error) {
       txnError = error;
     }
 
     if (txnError) {
       return {
         message: `Error while minting tokens to ${address}`,
-        detailedMessage: JSON.stringify(txnError)
-      }
+        details: JSON.stringify(txnError),
+      };
     }
 
     return {
       message: `Successfully minted ${amount} MTK to ${address}.`,
       transactionHash: mintTxnReceipt.transactionHash,
-      etherscanLink: `https://goerli.etherscan.io/tx/${ mintTxnReceipt.transactionHash}`
+      etherscan: `https://goerli.etherscan.io/tx/${mintTxnReceipt.transactionHash}`,
     };
   }
 
-  async delegate(delegateeAddress: string): Promise<TransactionResponseDTO | ErrorMessageDTO> {
+  async delegate(
+    delegateeAddress: string
+  ): Promise<TransactionResponseDTO | TransactionErrorDTO> {
     let txnError = null;
     let delegateTxnReceipt = null;
 
     try {
-      const delegateTxn = await this.myTokenContract.connect(this.signer).delegate(delegateeAddress);
+      const delegateTxn = await this.tokenContract
+        .connect(this.signer)
+        .delegate(delegateeAddress);
       delegateTxnReceipt = await delegateTxn.wait();
     } catch (error) {
       txnError = error;
@@ -111,63 +173,74 @@ export class AppService {
     if (txnError) {
       return {
         message: `Error delegating to ${delegateeAddress}`,
-        detailedMessage: JSON.stringify(txnError)
-      }
+        details: JSON.stringify(txnError),
+      };
     }
 
     return {
       message: `Successfully delegated votes to account address ${delegateeAddress}.`,
       transactionHash: delegateTxnReceipt.transactionHash,
-      etherscanLink: `https://goerli.etherscan.io/tx/${ delegateTxnReceipt.transactionHash}`
-    }
+      etherscan: `https://goerli.etherscan.io/tx/${delegateTxnReceipt.transactionHash}`,
+    };
   }
 
-  async vote(proposalId: string, amount: number): Promise<TransactionResponseDTO | ErrorMessageDTO> {
+  async vote(
+    proposalId: string,
+    amount: number
+  ): Promise<TransactionResponseDTO | TransactionErrorDTO> {
     let txnError = null;
     let voteTxnReceipt = null;
 
     try {
-      const voteTxn = await this.tokenizedBallotContract.connect(this.signer).vote(proposalId, amount);
+      const voteTxn = await this.ballotContract
+        .connect(this.signer)
+        .vote(proposalId, amount);
       voteTxnReceipt = await voteTxn.wait();
     } catch (error) {
       txnError = error;
     }
 
     if (txnError) {
-      console.log("🚀 ~ AppService ~ vote ~ txnError:", txnError)
-      
+      console.log('🚀 ~ AppService ~ vote ~ txnError:', txnError);
+
       return {
         message: `Error voting for proposal Id: ${proposalId}`,
-        detailedMessage: JSON.stringify(txnError)
-      }
+        details: JSON.stringify(txnError),
+      };
     }
 
     return {
       message: `Successfully voted for proposal with ID ${proposalId}.`,
       transactionHash: voteTxnReceipt.transactionHash,
-      etherscanLink: `https://goerli.etherscan.io/tx/${ voteTxnReceipt.transactionHash}`
-    }
+      etherscan: `https://goerli.etherscan.io/tx/${voteTxnReceipt.transactionHash}`,
+    };
   }
 
-  async getWinningProposal(): Promise<TransactionResponseDTO | ErrorMessageDTO> {
+  async getWinningProposal(): Promise<
+    TransactionResponseDTO | TransactionErrorDTO
+  > {
     let txnError = null;
     let winningProposal = -1;
     let proposalName = '';
     let proposalVoteCount = 0;
 
     try {
-      const winningProposalBN: BigNumber = await this.tokenizedBallotContract.connect(this.signer).winningProposal();
+      const winningProposalBN: BigNumber = await this.ballotContract
+        .connect(this.signer)
+        .winningProposal();
       const winningProposalStr = ethers.utils.formatEther(winningProposalBN);
       winningProposal = parseFloat(winningProposalStr);
 
-      const proposals = await this.tokenizedBallotContract.connect(this.signer).proposals(winningProposalBN);
+      const proposals = await this.ballotContract
+        .connect(this.signer)
+        .proposals(winningProposalBN);
       const proposalNameBytes32 = proposals[0];
       proposalName = ethers.utils.parseBytes32String(proposalNameBytes32);
-      
-      const proposalVoteCountBN = proposals[1];
-      const proposalVoteCountStr = ethers.utils.formatEther(proposalVoteCountBN);
-      proposalVoteCount = parseFloat(proposalVoteCountStr);
 
+      const proposalVoteCountBN = proposals[1];
+      const proposalVoteCountStr =
+        ethers.utils.formatEther(proposalVoteCountBN);
+      proposalVoteCount = parseFloat(proposalVoteCountStr);
     } catch (error) {
       txnError = error;
     }
@@ -175,14 +248,14 @@ export class AppService {
     if (txnError) {
       return {
         message: `Error while getting winning proposal.`,
-        detailedMessage: JSON.stringify(txnError)
-      }
+        details: JSON.stringify(txnError),
+      };
     }
-    
+
     return {
       message: `Successfully voted for proposal with ID: ${winningProposal}. Proposal name: ${proposalName}. Proposal vote count: ${proposalVoteCount}.`,
       transactionHash: 'No hash. It was reading operation.',
-      etherscanLink: `No etherscan link. It was reading operation.`
-    }
+      etherscan: `No etherscan link. It was reading operation.`,
+    };
   }
 }
