94a19a3be1dc0b23742af4a72e8baf49fcb6d65f
meeky <98189596+mmsaki@users.noreply.github.com>
2023-03-16 02:46:08 -0500

    fix: config load not needed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [
-        async () => {
-          return configuration;
-        },
-      ],
       cache: true,
     }),
   ],
